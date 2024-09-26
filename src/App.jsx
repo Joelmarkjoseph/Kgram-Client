aea1e920e469738171a292e43f49d5c06e139142
Joel Mark Joseph S <joelmarkjoseph2004@gmail.com>
2024-09-26 06:00:34 +0530

    New Commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Home from "../pages/home";
+import Home from "../pages/Home";
 import Gallery from "../pages/Gallery";
 import About from "../pages/About";
 import Login from "../pages/Login";
