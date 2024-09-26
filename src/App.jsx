a96a48a6d8349b08dac5513ae117d1a5b05d4ebd
Joel Mark Joseph S <joelmarkjoseph2004@gmail.com>
2024-09-26 12:14:11 +0530

    New COMMITS

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </Router>
     </>
